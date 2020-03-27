8adf3f5ac542c12dda60eb20c498bd3fffadbc12
Gustav <gustavw@live.dk>
2020-03-27 22:58:06 +0100

    login opdatering

--- src/main/java/PresentationLayer/Register.java
+++ src/main/java/PresentationLayer/Register.java
@@ -1,5 +1,6 @@
 package PresentationLayer;
 
+import FunctionLayer.ReturnedUseres;
 import FunctionLayer.Useres;
 import FunctionLayer.LogicFacade;
 import FunctionLayer.LoginSampleException;
@@ -7,6 +8,9 @@ import FunctionLayer.LoginSampleException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+
+import static DBAccess.UsersMapper.createUserList;
 
 public class Register extends Command {
 
@@ -15,6 +19,13 @@ public class Register extends Command {
         String email = request.getParameter( "email" );
         String password1 = request.getParameter( "password1" );
         String password2 = request.getParameter( "password2" );
+        ArrayList<ReturnedUseres> users = createUserList();
+
+       for(int i = 0; i < users.size(); i++){
+           if(users.get(i).getEmail().equals(email)){
+               throw new LoginSampleException("Der findes allerede en bruger med denne email.");
+           }
+       }
        if ( password1.equals( password2 ) ) {
             Useres useres = LogicFacade.createUser( email, password1 );
             HttpSession session = request.getSession();
@@ -25,7 +36,7 @@ public class Register extends Command {
             request.setAttribute("besked1", "Bruger oprettet!");
             return "index";
         } else {
-            throw new LoginSampleException( "the two passwords did not match" );
+            throw new LoginSampleException( "De to passwords er ikke ens." );
         }
     }
 
